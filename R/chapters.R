303e3a16781d6130b30ecb81bc6f16eb446c2db6
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-10-16 16:34:48 +0200

    Improved documentation

--- R/chapters.R
+++ R/chapters.R
@@ -1,3 +1,9 @@
+#' @title Load functions from a chapter
+#' @description Loads all the functions associated with a specific book chapter.
+#' @param chap_num Number of book chapter (from 2 to 10)
+#' @return Loads the functions from that chapter
+#' @author Waldir Leoncio
+#' @references https://stackoverflow.com/q/64388629/1169233
 #' @export
 chapter <- function(chap_num) {
 	if (missing(chap_num)) {
@@ -19,3 +25,5 @@ chapter <- function(chap_num) {
 		attach(list2env(chapters[[chap_num - 1]]))
 	}
 }
+# TODO: add unloading function?
+# TODO: address generated check notes (post question on StackOverflow?)
