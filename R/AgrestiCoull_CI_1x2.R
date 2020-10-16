055e21be8d7113889417d936481faea613500c84
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-10-16 17:08:53 +0200

    Added unloading function, updated docs

--- R/AgrestiCoull_CI_1x2.R
+++ R/AgrestiCoull_CI_1x2.R
@@ -10,7 +10,7 @@
 #' @param alpha the nominal level, e.g. 0.05 for 95% CIs
 #' @param printresults display results (0 = no, 1 = yes)
 #' @examples
-#' chapter(2)
+#' load_chapter(2)
 #' AgrestiCoull_CI_1x2(19)
 #' AgrestiCoull_CI_1x2(19, 20, .15)
 AgrestiCoull_CI_1x2 = function(X, n=NULL, alpha=0.05, printresults=TRUE) {
--- R/chapters.R
+++ R/chapters.R
@@ -5,7 +5,7 @@
 #' @author Waldir Leoncio
 #' @references https://stackoverflow.com/q/64388629/1169233
 #' @export
-chapter <- function(chap_num) {
+load_chapter <- function(chap_num) {
 	if (missing(chap_num)) {
 		stop("Please choose a chapter between 2 and 10.")
 	}
@@ -21,9 +21,57 @@ chapter <- function(chap_num) {
 				Wald_CI_1x2 = Wald_CI_1x2
 			)
 		)
-		message("Loading functions from chapter ", chap_num)
-		attach(list2env(chapters[[chap_num - 1]]))
+		chap_name <- paste("Chapter", chap_num)
+		is_attached <- any(grepl(pattern = chap_name, x = search()))
+		if (is_attached) {
+			message(
+				"Chapter functions already loaded. ",
+				"You can unload them with unload_chapter(", chap_num,
+				"). Exiting."
+			)
+		} else {
+			message("Loading functions from chapter ", chap_num)
+			attach(list2env(chapters[[chap_num - 1]]), name = chap_name)
+		}
+		# on.exit(detach(target))
 	}
 }
-# TODO: add unloading function?
+
+#' @title Unload functions from a chapter
+#' @description Unloads all the functions associated with a specific book chapter.
+#' @param chap_num Number of book chapter (from 2 to 10)
+#' @return Unloads the functions from that chapter
+#' @author Waldir Leoncio
+#' @export
+unload_chapter <- function(chap_num) {
+	if (missing(chap_num)) {
+		stop("Please choose a chapter between 2 and 10.")
+	}
+	if (chap_num < 2 | chap_num > 10) {
+		stop(
+			"There is no Chapter ", chap_num,
+			". Please choose a chapter between 2 and 10."
+		)
+	} else {
+		# TODO: this list is repeated. Merge!
+		chapters <- list(
+			ch2 = list(
+				AgrestiCoull_CI_1x2 = AgrestiCoull_CI_1x2,
+				Wald_CI_1x2 = Wald_CI_1x2
+			)
+		)
+		chap_name <- paste("Chapter", chap_num)
+		is_attached <- any(grepl(pattern = chap_name, x = search()))
+		if (!is_attached) {
+			message(
+				"Chapter functions not attached. ",
+				"Use load_chapter(", chap_num, ") to attach them. Exiting."
+			)
+		} else {
+			message("Unloading functions from chapter ", chap_num)
+			detach(eval(chap_name), character.only = TRUE)
+		}
+	}
+}
+
 # TODO: address generated check notes (post question on StackOverflow?)
