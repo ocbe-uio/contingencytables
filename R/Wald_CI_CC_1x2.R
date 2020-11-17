5983ba866d2700fd84eb95f31050130ae410e353
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-11-17 14:07:46 +0100

    Added original Wald_CI_CC_1x2()

---
+++ R/Wald_CI_CC_1x2.R
@@ -0,0 +1,49 @@
+# function [L, U, estimate] = Wald_CI_CC_1x2(X, n, alpha, printresults)
+
+Wald_CI_CC_1x2 = function(X, n, alpha=0.05, printresults=T) {
+
+    # The Wald confidence interval with continuity correction for the binomial probability
+    # Described in Chapter 2 "The 1x2 Table and the Binomial Distribution"
+    # 
+    # Input arguments
+    # ---------------
+    # X = the number of successes
+    # n = the total number of observations
+    # alpha: the nominal level, e.g. 0.05 for 95# CIs 
+    # printresults: display results (0 = no, 1 = yes)
+
+    if (missing(n)) {
+        X = 250; n = 533; # Example: The number of 1st order male births (Singh et al. 2010)
+     #   X = 204; n = 412; # Example: The number of 2nd order male births (Singh et al. 2010)
+     #   X = 103; n = 167; # Example: The number of 3rd order male births (Singh et al. 2010)
+     #   X = 33; n = 45;   # Example: The number of 4th order male births (Singh et al. 2010)
+     #   X = 13; n = 16;   # Example: Ligarden et al. (2010)
+    }
+
+    # Estimate of the binomial probability (pihat)
+    estimate = X/n
+
+    # The standard error of the estimate
+    SE = sqrt(estimate*(1 - estimate)/n)
+
+    # The upper alpha/2 percentile of the standard normal distribution
+    z = qnorm(1 - alpha/2, 0, 1)
+
+    # Calculate the confidence limits
+    L = estimate - z*SE - 1/(2*n)
+    U = estimate + z*SE + 1/(2*n)
+
+    # Overshoot can happen: truncate the results
+    L = max(0, L)
+    U = min(U, 1)
+
+    if (printresults) {
+        print(sprintf('The Wald CI with continuity correction: estimate = %6.4f (%g%% CI %6.4f to %6.4f)',
+            estimate, 100*(1 - alpha), L, U))
+    }
+
+    res = c(L, U, estimate)
+    names(res) = c("lower", "upper", "estimate")
+    invisible(res)
+
+}
