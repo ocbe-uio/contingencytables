c2bac4d1d49d3b36f22f813501d4bf9a4ba30993
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-12-08 15:47:06 +0100

    Fixed tests

--- tests/testthat/test-ch3.R
+++ tests/testthat/test-ch3.R
@@ -4,12 +4,16 @@ test_that("Chapter 3 functions basically work", {
 	load_chapter(3)
 	expect_output(
 		object = Chacko_test_1xc(n=c(1, 4, 3, 11, 9)),
-		expected = "The Chacko test: P = 0.00873, T = 9.482 (df = 2)"
+		regexp = "P = 0.00873, T = 9.482 \\(df = 2\\)"
 	)
 	expect_output(
 		object = Exact_multinomial_test_1xc(
 			n=c(6, 1, 3), pi0=c(0.402, 0.479, 0.119)
 		),
 		regexp = "P = 0.04792"
+	)
+	expect_output(
+		object = Gold_Wald_CIs_1xc(n=c(276, 380, 118)),
+		regexp = "pi_3: estimate = 0.1525 \\(0.1208 to 0.1841\\)"
 	)
 })
