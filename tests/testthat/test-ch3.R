b3979e7fb70ebcc42cdfcd12a65944a0a8e12d7d
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-12-08 15:02:24 +0100

    Added test

--- tests/testthat/test-ch3.R
+++ tests/testthat/test-ch3.R
@@ -6,4 +6,10 @@ test_that("Chapter 3 functions basically work", {
 		object = Chacko_test_1xc(n=c(1, 4, 3, 11, 9)),
 		expected = "The Chacko test: P = 0.00873, T = 9.482 (df = 2)"
 	)
+	expect_output(
+		object = Exact_multinomial_test_1xc(
+			n=c(6, 1, 3), pi0=c(0.402, 0.479, 0.119)
+		),
+		expected = ""
+	)
 })
