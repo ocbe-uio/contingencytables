64d1ec9fdbe7767c4b1b8d1c8010a62a9f3de69a
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-12-08 16:01:39 +0100

    Adapted Goodman_Wald_CIs_1xc

--- R/Goodman_Wald_CIs_1xc.R
+++ R/Goodman_Wald_CIs_1xc.R
@@ -1,44 +1,43 @@
-Goodman_Wald_CIs_1xc = function(n, alpha=0.05, printresults=T) {
+#' @title The Goodman Wald simultaneous intervals for the multinomial probabilities
+#' @description The Goodman Wald simultaneous intervals for the multinomial probabilities
+#' @description (with Bonferroni adjustment)
+#' @description Described in Chapter 3 "The 1xc Table and the Multinomial Distribution"
+#' @param n the observed counts (a 1xc vector, where c is the number of categories)
+#' @param alpha the nominal level, e.g. 0.05 for 95# CIs
+#' @param printresults display results (F = no, T = yes)
+#' @examples load_chapter(3)
+#' # Genotype counts for SNP rs 6498169 in RA patients
+#' Goodman_Wald_CIs_1xc(n=c(276, 380, 118))
+Goodman_Wald_CIs_1xc <- function(n, alpha=0.05, printresults=TRUE) {
+	c0 <- length(n)
+	N <- sum(n)
 
-    # function [L, U, pihat] = Goodman_Wald_CIs_1xc(n, alpha, printresults)
-    # The Goodman Wald simultaneous intervals for the multinomial probabilities
-    # (with Bonferroni adjustment)
-    # Described in Chapter 3 "The 1xc Table and the Multinomial Distribution"
-    #
-    # Input arguments
-    # ---------------
-    # n: the observed counts (a 1xc vector, where c is the number of categories)
-    # alpha: the nominal level, e.g. 0.05 for 95# CIs 
-    # printresults: display results (F = no, T = yes)
+	# Estimates of the multinomial probabilities
+	pihat <- n / N
 
-    if (missing(n)) {
-        n = c(276, 380, 118) # Example: Genotype counts for SNP rs 6498169 in RA patients
-    }
+	# Simultaneous confidence intervals with Bonferroni adjustment
+	L <- rep(0, c0)
+	U <- rep(0, c0)
+	Bonferroni <- qchisq(1 - alpha / c0, 1)
+	for (i in 1:c0) {
+		L[i] = pihat[i] - sqrt(Bonferroni * pihat[i] * (1 - pihat[i]) / N)
+		U[i] = pihat[i] + sqrt(Bonferroni * pihat[i] * (1 - pihat[i]) / N)
+	}
 
-    c0 = length(n)
-    N = sum(n)
+	if (printresults) {
+		print(sprintf('The Goodman Wald simultaneous intervals'), quote=FALSE)
+		for (i in 1:c0) {
+			print(
+				sprintf(
+					'  pi_%i: estimate = %6.4f (%6.4f to %6.4f)',
+					i, pihat[i], L[i], U[i]
+				),
+				quote=FALSE
+			)
+		}
+	}
 
-    # Estimates of the multinomial probabilities
-    pihat = n/N
+	res <- data.frame(lower=L, upper=U, estimate=pihat)
+	invisible(res)
 
-    # Simultaneous confidence intervals with Bonferroni adjustment
-    L = rep(0, c0)
-    U = rep(0, c0)
-    Bonferroni = qchisq(1-alpha/c0, 1)
-    for (i in 1:c0) {
-        L[i] = pihat[i] - sqrt(Bonferroni*pihat[i]*(1-pihat[i])/N)
-        U[i] = pihat[i] + sqrt(Bonferroni*pihat[i]*(1-pihat[i])/N)
-    }
-
-    if (printresults) {
-        print(sprintf('The Goodman Wald simultaneous intervals'), quote=F)
-        for (i in 1:c0) {
-            print(sprintf('  pi_%i: estimate = %6.4f (%6.4f to %6.4f)', 
-                i, pihat[i], L[i], U[i]), quote=F)
-        }
-    }
-    
-    res = data.frame(lower=L, upper=U, estimate=pihat)
-    invisible(res)
-    
 }
--- tests/testthat/test-ch3.R
+++ tests/testthat/test-ch3.R
@@ -16,4 +16,8 @@ test_that("Chapter 3 functions basically work", {
 		object = Gold_Wald_CIs_1xc(n=c(276, 380, 118)),
 		regexp = "pi_3: estimate = 0.1525 \\(0.1208 to 0.1841\\)"
 	)
+	expect_output(
+		object =  Goodman_Wald_CIs_1xc(n=c(276, 380, 118)),
+		regexp = "pi_3: estimate = 0.1525 \\(0.1215 to 0.1834\\)"
+	)
 })
