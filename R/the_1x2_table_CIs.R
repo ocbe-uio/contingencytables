356dae504337a2668ce26f283dc479e62b887926
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-12-08 10:57:37 +0100

    Improved formatting

--- R/the_1x2_table_CIs.R
+++ R/the_1x2_table_CIs.R
@@ -1,5 +1,8 @@
 #' @title The 1x2 Table CIs
-#' @examples load_chapter()
+#' @param X the number of successes
+#' @param n the total number of observations
+#' @param alpha the nominal level, e.g. 0.05 for 95% CIs
+#' @examples load_chapter(2)
 #' # The number of 1st order male births (Singh et al. 2010)
 #' the_1x2_table_CIs(X=250, n=533)
 #' # The number of 2nd order male births (Singh et al. 2010)
@@ -18,57 +21,95 @@ the_1x2_table_CIs <- function(X, n, alpha=0.05) {
 	# ======================================================== #
 	# Ad-hoc function to print output                          #
 	# ======================================================== #
-	myprint <- function(txt, ...) print(sprintf(txt, ...), quote=FALSE)
+	myprint <- function(txt, ...) cat(sprintf(txt, ...), "\n")
 
 	# ======================================================== #
 	# Output                                                   #
 	# ======================================================== #
 
-	print(sprintf('Estimate of pi: %i / %i = %5.3f', X, n, estimate), quote=FALSE)
+	myprint('Estimate of pi: %i / %i = %5.3f', X, n, estimate)
 
-	print(sprintf('Interval method                  %i%% CI        width', 100 * (1-alpha)), quote=FALSE)
-	print('----------------------------------------------------', quote=FALSE)
+	myprint(
+		'Interval method                  %i%% CI        width', 100 * (1-alpha)
+	)
+	myprint('----------------------------------------------------')
 
 	res <- Wald_CI_1x2(X, n, alpha, F)
-	print(sprintf('Wald                         %5.3f to %5.3f %8.3f', res[1], res[2], res[2] - res[1]), quote=FALSE)
+	myprint(
+		'Wald                         %5.3f to %5.3f %8.3f',
+		res[1], res[2], res[2] - res[1]
+	)
 
 	res <- Wald_CI_CC_1x2(X, n, alpha, F)
-	print(sprintf('Wald with CC                 %5.3f to %5.3f %8.3f', res[1], res[2], res[2] - res[1]), quote=FALSE)
+	myprint(
+		'Wald with CC                 %5.3f to %5.3f %8.3f',
+		res[1], res[2], res[2] - res[1]
+	)
 
 	res <- LR_CI_1x2(X, n, alpha, F)
-	print(sprintf('Likelihood ratio             %5.3f to %5.3f %8.3f', res[1], res[2], res[2] - res[1]), quote=FALSE)
+	myprint(
+		'Likelihood ratio             %5.3f to %5.3f %8.3f',
+		res[1], res[2], res[2] - res[1]
+	)
 
 	res <- Wilson_score_CI_1x2(X, n, alpha, F)
-	print(sprintf('Wilson score                 %5.3f to %5.3f %8.3f', res[1], res[2], res[2] - res[1]), quote=FALSE)
+	myprint(
+		'Wilson score                 %5.3f to %5.3f %8.3f',
+		res[1], res[2], res[2] - res[1]
+	)
 
 	res <- Wilson_score_CI_CC_1x2(X, n, alpha, F)
-	print(sprintf('Wilson score with CC         %5.3f to %5.3f %8.3f', res[1], res[2], res[2] - res[1]), quote=FALSE)
+	myprint(
+		'Wilson score with CC         %5.3f to %5.3f %8.3f',
+		res[1], res[2], res[2] - res[1]
+	)
 
 	res <- AgrestiCoull_CI_1x2(X, n, alpha, F)
-	print(sprintf('Agresti-Coull                %5.3f to %5.3f %8.3f', res[1], res[2], res[2] - res[1]), quote=FALSE)
+	myprint(
+		'Agresti-Coull                %5.3f to %5.3f %8.3f',
+		res[1], res[2], res[2] - res[1]
+	)
 
 	res <- Jeffreys_CI_1x2(X, n, alpha, F)
-	print(sprintf('Jeffreys                     %5.3f to %5.3f %8.3f', res[1], res[2], res[2] - res[1]), quote=FALSE)
+	myprint(
+		'Jeffreys                     %5.3f to %5.3f %8.3f',
+		res[1], res[2], res[2] - res[1]
+	)
 
 	res <- Arcsine_CI_1x2(X, n, alpha, F)
-	print(sprintf('Arcsine (Anscombe)           %5.3f to %5.3f %8.3f', res[1], res[2], res[2] - res[1]), quote=FALSE)
+	myprint(
+		'Arcsine (Anscombe)           %5.3f to %5.3f %8.3f',
+		res[1], res[2], res[2] - res[1]
+	)
 
 	res <- ClopperPearson_exact_CI_1x2(X, n, alpha, F)
-	print(sprintf('Clopper-Pearson exact        %5.3f to %5.3f %8.3f', res[1], res[2], res[2] - res[1]), quote=FALSE)
+	myprint(
+		'Clopper-Pearson exact        %5.3f to %5.3f %8.3f',
+		res[1], res[2], res[2] - res[1]
+	)
 
 	#res = ClopperPearson_exact_CI_1x2_beta_version(X, n, alpha, F)
-	#print(sprintf('Clopper-Pearson exact (beta) %5.3f to %5.3f %8.3f', res[1], res[2], res[2] - res[1]), quote=FALSE)
+	#myprint('Clopper-Pearson exact (beta) %5.3f to %5.3f %8.3f', res[1], res[2], res[2] - res[1])
 
 	res <- Blaker_exact_CI_1x2(X, n, alpha, F)
-	print(sprintf('Blaker exact                 %5.3f to %5.3f %8.3f', res[1], res[2], res[2] - res[1]), quote=FALSE)
+	myprint(
+		'Blaker exact                 %5.3f to %5.3f %8.3f',
+		res[1], res[2], res[2] - res[1]
+	)
 
 	res <- ClopperPearson_midP_CI_1x2(X, n, alpha, F)
-	print(sprintf('Clopper-Pearson mid-p        %5.3f to %5.3f %8.3f', res[1], res[2], res[2] - res[1]), quote=FALSE)
+	myprint(
+		'Clopper-Pearson mid-p        %5.3f to %5.3f %8.3f',
+		res[1], res[2], res[2] - res[1]
+	)
 
 	res <- Blaker_midP_CI_1x2(X, n, alpha, F)
-	print(sprintf('Blaker mid-P                 %5.3f to %5.3f %8.3f', res[1], res[2], res[2] - res[1]), quote=FALSE)
+	myprint(
+		'Blaker mid-P                 %5.3f to %5.3f %8.3f',
+		res[1], res[2], res[2] - res[1]
+	)
 
-	print('----------------------------------------------------', quote=FALSE)
-	print('CC = continuity correction', quote=FALSE)
+	myprint('----------------------------------------------------')
+	myprint('CC = continuity correction')
 
 }
