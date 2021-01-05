8473df0b796b0ca28e2287a2f21390fb4b58441f
Waldir Leoncio <waldir.leoncio@gmail.com>
2021-01-05 09:58:16 +0100

    Added tests

--- tests/testthat/test-ch4.R
+++ tests/testthat/test-ch4.R
@@ -2,9 +2,14 @@ context("Chapter 4")
 
 test_that("Chapter 4 functions basically work", {
 	load_chapter(4)
-	n <- matrix(c(9,4,4,10), nrow=2, byrow=T)
+	n <- matrix(c(9, 4, 4, 10), nrow=2, byrow=T)
 	expect_output(
 		object = Adjusted_inv_sinh_CI_OR_2x2(n),
-		regexp = " estimate = 5.6250 \\(95% CI 1.1414 to 21.7873\\)"
+		regexp = "estimate = 5.6250 \\(95% CI 1.1414 to 21.7873\\)"
+	)
+	n <- matrix(c(0,16,15,57), nrow=2, byrow=T)
+	expect_output(
+		object = Adjusted_inv_sinh_CI_ratio_2x2(n),
+		regexp = "estimate = 0.0000 \\(95% CI 0.0000 to 1.1524\\)"
 	)
 })
