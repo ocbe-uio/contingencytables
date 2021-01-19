3770a0d78e7f5821bcf4076a03bdd39a252d721a
Waldir Leoncio <waldir.leoncio@gmail.com>
2021-01-19 08:44:33 +0100

    Added unit test

--- tests/testthat/test-ch4.R
+++ tests/testthat/test-ch4.R
@@ -17,4 +17,8 @@ test_that("Chapter 4 functions basically work", {
 		object = Adjusted_log_CI_2x2(n),
 		regexp = "estimate = 7.0000 \\(95% CI 0.9241 to 27.0523\\)"
 	)
+	expect_output(
+		object = Wald_CI_2x2(n = matrix(c(7,27,1,33), nrow=2, byrow=T)),
+		regexp = "estimate = 0.1765 \\(95% CI 0.0292 to 0.3238\\)"
+	)
 })
