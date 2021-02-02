4b58e38f979bdfc7ced2e823264564fcdb6355f6
Waldir Leoncio <waldir.leoncio@gmail.com>
2021-02-02 09:59:19 +0100

    Added original script

---
+++ R/Inv_sinh_CI_ratio_2x2.R
@@ -0,0 +1,54 @@
+Inv_sinh_CI_ratio_2x2 = function(n, alpha=0.05, printresults=T) {
+
+    # function [L, U, estimate] = Inv_sinh_CI_ratio_2x2(n, alpha, printresults)
+    # The inverse hyperbolic sine confidence interval for the ratio of probabilities
+    # Described in Chapter 4 "The 2x2 Table"
+    # 
+    # Input arguments
+    # ---------------
+    # n: the observed counts (a 2x2 matrix)
+    # alpha: the nominal level, e.g. 0.05 for 95% CIs 
+    # printresults: display results (F = no, T = yes)
+
+    if (missing(n)) {
+      # An RCT of high vs standard dose of epinephrine (Perondi et al., 2004):
+        n = matrix(c(7,27,1,33), nrow=2, byrow=T) 
+      # The association between CHRNA4 genotype and XFS (Ritland et al., 2007): 
+      # n = matrix(c(0,16,15,57), nrow=2, byrow=T) 
+    } 
+
+    n1p = n[1,1] + n[1,2]
+    n2p = n[2,1] + n[2,2]
+
+    # Estimates of the two probabilities of success
+    pi1hat = n[1,1]/n1p
+    pi2hat = n[2,1]/n2p
+
+    # Estimate of the ratio of probabilities (phihat)
+    estimate = pi1hat/pi2hat
+
+    # The upper alpha/2 percentile of the standard normal distribution
+    z = qnorm(1-alpha/2, 0, 1)
+
+    # Calculate the confidence limits
+    tmp = asinh(0.5*z*sqrt(1/n[1,1] + 1/n[2,1] - 1/n1p - 1/n2p))
+    L = exp(log(estimate) - 2*tmp)
+    U = exp(log(estimate) + 2*tmp)
+
+    # Fix zero cell cases
+    if (n[1,1] == 0) {
+        U = ((z^2)/n1p)/(n[2,1]/n2p)
+    }
+    if (n[2,1] == 0) {
+        L = (n[1,1]/n1p)/((z^2)/n2p)
+    }
+
+    if (printresults) {
+        print(sprintf('The inverse sinh CI: estimate = %6.4f (%g%% CI %6.4f to %6.4f)', 
+            estimate, 100*(1 - alpha), L, U), quote=F)
+    }
+    
+    res = data.frame(lower=L, upper=U, estimate=estimate)
+    invisible(res)          
+
+}
