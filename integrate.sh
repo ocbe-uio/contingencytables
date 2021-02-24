2cedc8a807da4f568395aee46ca54be99da6c9e7
Waldir Leoncio <waldir.leoncio@gmail.com>
2021-02-24 09:10:10 +0100

    Added integration Shell script

---
+++ integrate.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# This script automates the preparation of a script file for integrating the package.
+
+echo 'Moving file from aux/ to R/'
+path_filename=$1
+mv -v $path_filename R/
+
+filename_ext=$(echo $path_filename | cut -d '/' -f 3)
+filename=$(echo $filename_ext | cut -d '.' -f 1)
+
+echo "Creating $filename branch"
+git branch $filename
+
+git checkout $filename
+
+echo "Committing original script"
+git add -A
+git commit -m "Added original script"
