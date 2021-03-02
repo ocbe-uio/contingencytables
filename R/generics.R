0fff0707d3b90433998ea68abd0e75eeb80bd7a4
Waldir Leoncio <waldir.leoncio@gmail.com>
2021-03-02 10:18:29 +0100

    Refactoring

--- R/generics.R
+++ R/generics.R
@@ -16,8 +16,7 @@ ML_estimates <- function(...) {
 	UseMethod("ML_estimates")
 }
 
-score_test_statistic <- function(...)
-{
+score_test_statistic <- function(...) {
 	UseMethod("score_test_statistic")
 }
 
@@ -26,8 +25,7 @@ score_test_statistic <- function(...)
 # ======================================================== #
 
 calculate_limit_lower.Mee <- function(delta0, n11, n21, n1p, n2p, pi1hat,
-										pi2hat, alpha)
-{
+										pi2hat, alpha) {
 	ml.res = ML_estimates.Mee(n11, n21, n1p, n2p, delta0)
 	T0 <- score_test_statistic.Mee(
 		pi1hat, pi2hat, delta0, ml.res$p1hat, ml.res$p2hat, n1p, n2p
@@ -41,8 +39,7 @@ calculate_limit_lower.Mee <- function(delta0, n11, n21, n1p, n2p, pi1hat,
 }
 
 calculate_limit_upper.Mee <- function(delta0, n11, n21, n1p, n2p, pi1hat,
-										pi2hat, alpha)
-{
+										pi2hat, alpha) {
 	ml.res = ML_estimates.Mee(n11, n21, n1p, n2p, delta0)
 	T0 <- score_test_statistic.Mee(
 		pi1hat, pi2hat, delta0, ml.res$p1hat, ml.res$p2hat, n1p, n2p
@@ -70,8 +67,7 @@ ML_estimates.Mee <- function(n11, n21, n1p, n2p, delta0) {
 }
 
 score_test_statistic.Mee <- function(pi1hat, pi2hat, delta0, p1hat, p2hat, n1p,
-										n2p)
-{
+										n2p) {
 	T0 <- (pi1hat - pi2hat - delta0) / sqrt(p1hat * (1 - p1hat) / n1p + p2hat *
 		(1 - p2hat) / n2p)
 	return(T0)
@@ -82,8 +78,7 @@ score_test_statistic.Mee <- function(pi1hat, pi2hat, delta0, p1hat, p2hat, n1p,
 # ======================================================== #
 
 calculate_limit_lower.Koopman <- function(phi0, n11, n21, n1p, n2p, pi1hat,
-											pi2hat, alpha)
-{
+											pi2hat, alpha) {
 	ml.res = ML_estimates.Koopman(n11, n21, n1p, n2p, phi0)
 	T0 <- score_test_statistic.Koopman(
 		pi1hat, pi2hat, ml.res$p1hat, ml.res$p2hat, n1p, n2p, phi0
@@ -96,8 +91,7 @@ calculate_limit_lower.Koopman <- function(phi0, n11, n21, n1p, n2p, pi1hat,
 }
 
 calculate_limit_upper.Koopman <- function(phi0, n11, n21, n1p, n2p, pi1hat,
-											pi2hat, alpha)
-{
+											pi2hat, alpha) {
 	ml.res = ML_estimates.Koopman(n11, n21, n1p, n2p, phi0)
 	T0 <- score_test_statistic.Koopman(
 		pi1hat, pi2hat, ml.res$p1hat, ml.res$p2hat, n1p, n2p, phi0
@@ -109,8 +103,6 @@ calculate_limit_upper.Koopman <- function(phi0, n11, n21, n1p, n2p, pi1hat,
 	return(f)
 }
 
-
-# ==============================================================
 ML_estimates.Koopman <- function(n11, n21, n1p, n2p, phi0) {
 	A0 <- (n1p + n2p) * phi0
 	B0 <- -(n1p * phi0 + n11 + n2p + n21 * phi0)
@@ -121,10 +113,8 @@ ML_estimates.Koopman <- function(n11, n21, n1p, n2p, phi0) {
 	return(res)
 }
 
-# ============================================================================
 score_test_statistic.Koopman <- function(pi1hat, pi2hat, p1hat, p2hat, n1p,
-											n2p, phi0)
-{
+											n2p, phi0) {
 	T0 <- (pi1hat - phi0 * pi2hat) / sqrt(p1hat * (1 - p1hat) / n1p +
 		(phi0 ^ 2) * p2hat * (1 - p2hat) / n2p)
 	return(T0)
@@ -134,7 +124,6 @@ score_test_statistic.Koopman <- function(pi1hat, pi2hat, p1hat, p2hat, n1p,
 # Methods for Mettinen-Nurminen difference                 #
 # ======================================================== #
 
-# ==================================
 calculate_limit_lower.Mettinen_diff <- function(delta0, n11, n21, n1p, n2p, pi1hat, pi2hat, alpha) {
 	# global n11 n21 n1p n2p alphaglobal pi1hat pi2hat limit
 	ml.res = ML_estimates.Mettinen_diff(n11, n21, n1p, n2p, delta0)
@@ -147,7 +136,6 @@ calculate_limit_lower.Mettinen_diff <- function(delta0, n11, n21, n1p, n2p, pi1h
 	return(f)
 }
 
-# ==================================
 calculate_limit_upper.Mettinen_diff <- function(delta0, n11, n21, n1p, n2p, pi1hat, pi2hat, alpha) {
 	# global n11 n21 n1p n2p alphaglobal pi1hat pi2hat limit
 	ml.res = ML_estimates.Mettinen_diff(n11, n21, n1p, n2p, delta0)
@@ -160,8 +148,6 @@ calculate_limit_upper.Mettinen_diff <- function(delta0, n11, n21, n1p, n2p, pi1h
 	return(f)
 }
 
-# ================================================================
-# function [p1hat, p2hat] = ML_estimates(n11, n21, n1p, n2p, delta0)
 ML_estimates.Mettinen_diff <- function(n11, n21, n1p, n2p, delta0) {
 	L3 <- n1p + n2p
 	L2 <- (n1p + 2 * n2p) * delta0 - (n1p + n2p) - (n11 + n21)
@@ -176,8 +162,6 @@ ML_estimates.Mettinen_diff <- function(n11, n21, n1p, n2p, delta0) {
 	return(res)
 }
 
-
-# ===============================================================================
 score_test_statistic.Mettinen_diff <- function(pi1hat, pi2hat, delta0, p1hat, p2hat, n1p, n2p) {
 	T0 <- (pi1hat - pi2hat - delta0) / sqrt(p1hat * (1 - p1hat) / n1p + p2hat * (1 - p2hat) / n2p)
 	T0 <- T0 * sqrt(1 - 1 / (n1p + n2p))
