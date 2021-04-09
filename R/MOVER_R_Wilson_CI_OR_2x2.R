efc3a2501fe3ee4bcd1b4a3583d8eb250cc8bbae
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-04-09 08:41:47 +0200

    Fixed syntax

--- R/MOVER_R_Wilson_CI_OR_2x2.R
+++ R/MOVER_R_Wilson_CI_OR_2x2.R
@@ -5,18 +5,15 @@
 #' @param alpha the nominal level, e.g. 0.05 for 95% CIs
 #' @param printresults display results (F = no, 1T= yes)
 #' @examples load_chapter(4)
-#' A case-control study of GADA exposure on IPEX syndrome (Lampasona et al., 2013):
+#' # A case-control study of GADA exposure on IPEX syndrome (Lampasona et al., 2013):
 #' n <- matrix(c(9,4,4,10), nrow=2, byrow=TRUE)
-#' Wilson_score_CI_1x2.R")(n)
+#' MOVER_R_Wilson_CI_OR_2x2(n)
 #'
-#' The association between CHRNA4 genotype and XFS (Ritland et al., 2007):
+#' # The association between CHRNA4 genotype and XFS (Ritland et al., 2007):
 #' n <- matrix(c(0,16,15,57), nrow=2, byrow=TRUE)
-#' Wilson_score_CI_1x2.R")(n)
+#' MOVER_R_Wilson_CI_OR_2x2(n)
 #'
 #' unload_chapter(4)
-#' @param [~, U_tmp, ~] MOVER_R_Wilson_CI_OR_2x2([0, n1p n[2, 2], n[2, 1]], alpha, 0)
-source(" / Users / ole / Documents / Petter Laake / chap2_translation / Wilson_score_CI_1x2.R")
-
 MOVER_R_Wilson_CI_OR_2x2 <- function(n, alpha=0.05, printresults=TRUE) {
 
 	# If n_22 = 0, use an equivalent case, which gives a finite upper limit
@@ -41,18 +38,18 @@ MOVER_R_Wilson_CI_OR_2x2 <- function(n, alpha=0.05, printresults=TRUE) {
 	# The estimated logits of pi_1 and pi_2 and their CIs
 	q1hat <- pi1hat / (1 - pi1hat)
 	q2hat <- pi2hat / (1 - pi2hat)
-	L1 <- res1$lower / (1 - res1$lower)
-	L2 <- res2$lower / (1 - res2$lower)
-	U1 <- res1$upper / (1 - res1$upper)
-	U2 <- res2$upper / (1 - res2$upper)
+	L1 <- res1["lower"] / (1 - res1["lower"])
+	L2 <- res2["lower"] / (1 - res2["lower"])
+	U1 <- res1["upper"] / (1 - res1["upper"])
+	U2 <- res2["upper"] / (1 - res2["upper"])
 
 	# Calculate the confidence limits
 	L <- (q1hat * q2hat - sqrt((q1hat * q2hat) ^ 2 -
-		res1$lower * res2$upper * (2 * q1hat - res1$lower) * (2 * q2hat - res2$upper)))/
-		(res2$upper * (2 * q2hat - res2$upper))
+		res1["lower"] * res2["upper"] * (2 * q1hat - res1["lower"]) * (2 * q2hat - res2["upper"])))/
+		(res2["upper"] * (2 * q2hat - res2["upper"]))
 	U <- (q1hat * q2hat + sqrt((q1hat * q2hat) ^ 2 -
-		res1$upper * res2$lower * (2 * q1hat - res1$upper) * (2 * q2hat - res2$lower)))/
-		(res2$lower * (2 * q2hat - res2$lower))
+		res1["upper"] * res2["lower"] * (2 * q1hat - res1["upper"]) * (2 * q2hat - res2["lower"])))/
+		(res2["lower"] * (2 * q2hat - res2["lower"]))
 
 	# In case of n_12 = 0, let L = 1 / U_tmp, where U_tmp comes from the CI for 0 / n1+ vs n22 / n2+
 	if (n[1, 2] == 0) {
