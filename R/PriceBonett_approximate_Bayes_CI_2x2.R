0548b1b9191dd2a4749e7a45de2c2c3e317c18a7
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-04-09 09:41:40 +0200

    Reformatted function

--- R/PriceBonett_approximate_Bayes_CI_2x2.R
+++ R/PriceBonett_approximate_Bayes_CI_2x2.R
@@ -1,53 +1,53 @@
-PriceBonett_approximate_Bayes_CI_2x2 = function(n, a=1.25, b=2.5, alpha=0.05, printresults=T) {
+#' @title The Price-Bonett approximate Bayes confidence interval for the ratio of probabilities
+#' @description The Price-Bonett approximate Bayes confidence interval for the ratio of probabilities
+#' @description Described in Chapter 4 "The 2x2 Table"
+#' @param n the observed counts (a 2x2 matrix)
+#' @param a,b parameters of the beta distribution
+#' @param alpha the nominal level, e.g. 0.05 for 95# CIs
+#' @param printresults display results (0 = no, 1 = yes)
+#' @examples load_chapter(4)
+#' # An RCT of high vs standard dose of epinephrine (Perondi et al., 2004)
+#' n <- matrix(c(7, 27, 1, 33), nrow=2, byrow=TRUE)
+#' PriceBonett_approximate_Bayes_CI_2x2(n)
+#'
+#' # The association between CHRNA4 genotype and XFS (Ritland et al., 2007)
+#' n <- matrix(c(0, 16, 15, 57), nrow=2, byrow=TRUE)
+#' PriceBonett_approximate_Bayes_CI_2x2(n)
+#'
+#' unload_chapter(4)
+PriceBonett_approximate_Bayes_CI_2x2 <- function(n, a=1.25, b=2.5, alpha=0.05, printresults=TRUE) {
+	n1p <- n[1, 1] + n[1, 2]
+	n2p <- n[2, 1] + n[2, 2]
 
-    # function [L, U, estimate] = PriceBonett_approximate_Bayes_CI_2x2(n, a, b, alpha, printresults)
-    # The Price-Bonett approximate Bayes confidence interval for the ratio of probabilities
-    # Described in Chapter 4 "The 2x2 Table"
-    # 
-    # Input arguments
-    # ---------------
-    # n: the observed counts (a 2x2 matrix)
-    # a,b: parameters of the beta distribution
-    # alpha: the nominal level, e.g. 0.05 for 95# CIs 
-    # printresults: display results (0 = no, 1 = yes)
+	# Estimates of the two probabilities of success
+	pi1hat <- n[1, 1] / n1p
+	pi2hat <- n[2, 1] / n2p
 
-    if (missing(n)) {
-        n = matrix(c(7, 27, 1, 33), nrow=2, byrow=T)  # An RCT of high vs standard dose of epinephrine (Perondi et al., 2004)
-        # n = matrix(c(0, 16, 15, 57), nrow=2, byrow=T) # The association between CHRNA4 genotype and XFS (Ritland et al., 2007)
-    }
+	# Estimate of the ratio of probabilities (phihat)
+	estimate <- pi1hat / pi2hat
 
-    n1p = n[1,1] + n[1,2]
-    n2p = n[2,1] + n[2,2]
+	# Approximation of log(pi_1 / pi_2) has mean log_phi_tilde...
+	pi1tilde <- (n[1, 1] + a - 1) / (n1p + a + b - 2)
+	pi2tilde <- (n[2, 1] + a - 1) / (n2p + a + b - 2)
+	log_phi_tilde <- log(pi1tilde) - log(pi2tilde)
 
-    # Estimates of the two probabilities of success
-    pi1hat = n[1,1]/n1p
-    pi2hat = n[2,1]/n2p
+	# ... and variance var_log_phi_tilde
+	tmp1 <- n[1, 1] + a - 1 + ((n[1, 1] + a - 1) ^ 2) / (n1p - n[1, 1] + b - 1)
+	tmp2 <- n[2, 1] + a - 1 + ((n[2, 1] + a - 1) ^ 2) / (n2p - n[2, 1] + b - 1)
+	var_log_phi_tilde <- 1 / tmp1 + 1 / tmp2
 
-    # Estimate of the ratio of probabilities (phihat)
-    estimate = pi1hat/pi2hat
+	# The upper alpha / 2 percentile of the standard normal distribution
+	z <- qnorm(1-alpha / 2, 0, 1)
 
-    # Approximation of log(pi_1/pi_2) has mean log_phi_tilde...
-    pi1tilde = (n[1,1] + a - 1)/(n1p + a + b - 2)
-    pi2tilde = (n[2,1] + a - 1)/(n2p + a + b - 2)
-    log_phi_tilde = log(pi1tilde) - log(pi2tilde)
+	# Calculate the confidence limits
+	L <- exp(log_phi_tilde - z * sqrt(var_log_phi_tilde))
+	U <- exp(log_phi_tilde + z * sqrt(var_log_phi_tilde))
 
-    # ... and variance var_log_phi_tilde
-    tmp1 = n[1,1] + a - 1 + ((n[1,1] + a - 1)^2)/(n1p - n[1,1] + b - 1)
-    tmp2 = n[2,1] + a - 1 + ((n[2,1] + a - 1)^2)/(n2p - n[2,1] + b - 1)
-    var_log_phi_tilde = 1/tmp1 + 1/tmp2
+	if (printresults) {
+		print(sprintf('The Price-Bonett approximate Bayes CI: estimate = %6.4f (%g%% CI %6.4f to %6.4f)',
+			estimate, 100 * (1 - alpha), L, U), quote=FALSE)
+	}
 
-    # The upper alpha/2 percentile of the standard normal distribution
-    z = qnorm(1-alpha/2, 0, 1)
-
-    # Calculate the confidence limits
-    L = exp(log_phi_tilde - z*sqrt(var_log_phi_tilde))
-    U = exp(log_phi_tilde + z*sqrt(var_log_phi_tilde))
-
-    if (printresults) {
-        print(sprintf('The Price-Bonett approximate Bayes CI: estimate = %6.4f (%g%% CI %6.4f to %6.4f)', 
-            estimate, 100*(1 - alpha), L, U), quote=F)
-    }
-
-    res = data.frame(lower=L, upper=U, estimate=estimate)
-    invisible(res)    
+	res <- data.frame(lower=L, upper=U, estimate=estimate)
+	invisible(res)
 }
