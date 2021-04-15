abbcca8edd1c55cab3dcb468e099895be5f6d5bd
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-04-15 10:04:32 +0200

    Added unit tests

---
+++ tests/testthat/test-ch5.R
@@ -0,0 +1,16 @@
+context("Chapter 5")
+
+test_that("Chapter 8 functions basically work", {
+	load_chapter(5)
+	n <- rbind(c(8, 53), c(10, 48), c(11, 100), c(22, 102), c(6, 129))
+	a <- c(1, 2, 3, 4, 5)
+	expect_output(
+		object = CochranArmitage_CI_rx2(n, a),
+		regexp = "betahat = -0.0194 \\(95% CI -0.0395 to 0.0007\\)"
+	)
+	# expect_output(
+	# 	object = ,
+	# 	regexp = ""
+	# )
+	unload_chapter(5)
+})
