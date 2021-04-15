b1e0461b728e2706b10da76978fbb06787d1a248
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-04-15 10:38:34 +0200

    Added function to chapter list

--- R/chapters.R
+++ R/chapters.R
@@ -160,7 +160,9 @@ list_chapter_functions <- function() {
 		the_2x2_table_tests                = the_2x2_table_tests
 	)
 	ch5 <- list(
-		CochranArmitage_CI_rx2 = CochranArmitage_CI_rx2
+		CochranArmitage_CI_rx2 = CochranArmitage_CI_rx2,
+		CochranArmitage_exact_cond_midP_tests_rx2 =
+			CochranArmitage_exact_cond_midP_tests_rx2
 	)
 	ch6 <- list()
 	ch7 <- list()
