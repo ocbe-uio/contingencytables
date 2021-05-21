5383c7951a47f67739c1301304b943d649fb576d
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-05-21 10:35:21 +0200

    Reformatted function

--- R/the_rx2_table.R
+++ R/the_rx2_table.R
@@ -1,45 +1,42 @@
+#' @title The rx2 table
+#' @param n the total number of observations
+#' @param alpha the nominal level, e.g. 0.05 for 95% CIs#'
+#' @param direction the direction of the success probabilities
+#' ("increasing" or "decreasing")
+#' @examples
+#' load_chapter(5)
+#' # Alcohol consumption and malformations (Mills and Graubard, 1987)
+#' direction <- 'increasing'
+#' n <- n.0
+#' a <- a.0
+#' the_rx2_table(n, a, direction)
+#'
+#' # Elevated troponin T levels in stroke patients (Indredavik et al., 2008)
+#' direction <- 'decreasing'
+#' n <- n.1
+#' a <- a.0       # equally spaced scores
+#' a <- a.1    # mid-interval scores
+#' the_rx2_table(n, a, direction)
+#'
+#' unload_chapter(5)
+the_rx2_table <- function(n, alpha=0.05, direction="increasing") {
 
-path = getwd()
-source(paste(path, "CochranArmitage_CI_rx2.R", sep="/"))
-source(paste(path, "CochranArmitage_exact_cond_midP_tests_rx2.R", sep="/"))
-source(paste(path, "CochranArmitage_MH_tests_rx2.R", sep="/"))
-source(paste(path, "Exact_cond_midP_unspecific_ordering_rx2.R", sep="/"))
-source(paste(path, "Pearson_LR_tests_rxc.R", sep="/"))
-source(paste(path, "Pearson_LR_tests_unspecific_ordering_rx2.R", sep="/"))
-source(paste(path, "Trend_estimate_CI_tests_rx2.R", sep="/"))
-
-the_rx2_table = function(n, alpha=0.05, direction="increasing") {
-
-	n.0 = rbind(c(48, 17066), c(38, 14464), c(5, 788), c(1, 126), c(1, 37))
-	n.1 = rbind(c(8, 53), c(10, 48), c(11, 100), c(22, 102), c(6, 129))
-	a.0 = c(1, 2, 3, 4, 5)
-	a.1 = c(7, 22, 37, 48, 55)
-
-	if (missing(n)) {
-	    # Alcohol consumption and malformations (Mills and Graubard, 1987)
-		#     direction = 'increasing';
-		#     n = n.0
-		#     a = a.0
-
-	    # Elevated troponin T levels in stroke patients (Indredavik et al., 2008)
-	    direction = 'decreasing';
-	    n = n.1
-	    a = a.0       # equally spaced scores
-	    #  a = a.1    # mid-interval scores
-	}
-
+	n.0 <- rbind(c(48, 17066), c(38, 14464), c(5, 788), c(1, 126), c(1, 37))
+	n.1 <- rbind(c(8, 53), c(10, 48), c(11, 100), c(22, 102), c(6, 129))
+	a.0 <- c(1, 2, 3, 4, 5)
+	a.1 <- c(7, 22, 37, 48, 55)
 
 	.print('Method                          Statistic      P-value')
 	.print('-------------------------------------------------------')
 
 	.print('Tests for unordered alternatives')
-	results = Pearson_LR_tests_rxc(n, 0)
+	results <- Pearson_LR_tests_rxc(n, 0)
 	.print('  Pearson chi-squared         %6.3f (df=%g)   %8.5f', results$T_Pearson, results$df_Pearson, results$P_Pearson)
 	.print('  Likelihood ratio            %6.3f (df=%g)   %8.5f', results$T_LR, results$df_LR, results$P_LR)
 
 
 	.print('Tests for ordered alternatives')
-	results = Pearson_LR_tests_unspecific_ordering_rx2(n, direction, 0)
+	results <- Pearson_LR_tests_unspecific_ordering_rx2(n, direction, 0)
 	.print('  Pearson chi-squared         %6.3f (chibar) %8.5f', results$T_Pearson, results$P_Pearson)
 	.print('  Likelihood ratio            %6.3f (chibar) %8.5f', results$T_LR, results$P_LR)
 
@@ -48,14 +45,14 @@ the_rx2_table = function(n, alpha=0.05, direction="increasing") {
 	# for unspecific ordering
 	# De-comment the following lines to shortcut the calculations
 	if (all(n == n.0)) {
-	    P_Pearson = 0.02322; midP_Pearson = 0.02292; P_LR = 0.07186; midP_LR = 0.07156
+		P_Pearson <- 0.02322; midP_Pearson <- 0.02292; P_LR <- 0.07186; midP_LR <- 0.07156
 	} else if (all(n == n.1)) {
-	    P_Pearson = 0.00908; midP_Pearson = 0.00907; P_LR = 0.00364; midP_LR = 0.00363
+		P_Pearson <- 0.00908; midP_Pearson <- 0.00907; P_LR <- 0.00364; midP_LR <- 0.00363
 	} else {
-		tmp = Exact_cond_midP_unspecific_ordering_rx2(n, direction, 'Pearson', 0)
-		P_Pearson = tmp$P; midP_Pearson = tmp$midP
-	    tmp = Exact_cond_midP_unspecific_ordering_rx2(n, direction, 'LR', 0)
-	    P_LR = tmp$P; midP_LR = tmp$midP
+		tmp <- Exact_cond_midP_unspecific_ordering_rx2(n, direction, 'Pearson', 0)
+		P_Pearson <- tmp$P; midP_Pearson <- tmp$midP
+		tmp <- Exact_cond_midP_unspecific_ordering_rx2(n, direction, 'LR', 0)
+		P_LR <- tmp$P; midP_LR <- tmp$midP
 	}
 
 	.print('  Exact conditional (Pearson)                 %8.5f', P_Pearson)
@@ -64,12 +61,12 @@ the_rx2_table = function(n, alpha=0.05, direction="increasing") {
 	.print('  Mid-P (LR)                                  %8.5f', midP_LR)
 
 	.print('Tests for trend in the linear model')
-	results = CochranArmitage_MH_tests_rx2(n, a, 0)
+	results <- CochranArmitage_MH_tests_rx2(n, a, 0)
 	.print('  Cochran-Armitage            %6.3f          %8.5f', results$Z_CA, results$P_CA)
 	.print('  Modified Cochran-Armitage   %6.3f          %8.5f', results$Z_CA_mod, results$P_CA_mod)
 	.print('  Mantel-Haenszel             %6.3f          %8.5f', results$Z_MH, results$P_MH)
 
-	results_linear = Trend_estimate_CI_tests_rx2(n, a, 'identity', alpha, 0)
+	results_linear <- Trend_estimate_CI_tests_rx2(n, a, 'identity', alpha, 0)
 	.print('  Wald                        %6.3f          %8.5f', results_linear$Z_Wald, results_linear$P_Wald)
 	.print('  Likelihood ratio            %6.3f (df=%g)   %8.5f', results_linear$T_LR, results_linear$df_LR, results_linear$P_LR)
 
@@ -78,18 +75,18 @@ the_rx2_table = function(n, alpha=0.05, direction="increasing") {
 	.print('  Likelihood ratio (deviance) %6.3f (df=%g)   %8.5f', results_linear$D, results_linear$df_D, results_linear$P_D)
 
 	.print('Tests for trend in the logit model')
-	results_logit = Trend_estimate_CI_tests_rx2(n, a, 'logit', alpha, 0)
+	results_logit <- Trend_estimate_CI_tests_rx2(n, a, 'logit', alpha, 0)
 	.print('  Wald                        %6.3f          %8.5f', results_logit$Z_Wald, results_logit$P_Wald)
 	.print('  Likelihood ratio            %6.3f (df=%g)   %8.5f', results_logit$T_LR, results_logit$df_LR, results_logit$P_LR)
 
 	# A little bit of computation time for the Cochran-Armitage exact and mid-P tests
 	# De-comment the following lines to shortcut the calculations
 	if (all(n==n.0) &&  all(a==a.0)) {
-	    P = 0.20912; midP = 0.18116
+	    P <- 0.20912; midP <- 0.18116
 	} else if (all(n==n.1) && all(a==a.0)) {
-	    P = 0.08579; midP = 0.07719
+	    P <- 0.08579; midP <- 0.07719
 	} else if (all(n==n.1) && all(a==a.1)) {
-	    P = 0.14524; midP = 0.14404
+	    P <- 0.14524; midP <- 0.14404
 	}
 
 	.print('  Cochran-Armitage exact cond.                %8.5f', P)
@@ -103,10 +100,10 @@ the_rx2_table = function(n, alpha=0.05, direction="increasing") {
 	.print('Method                  Estimate         %g%% CI           Width', 100*(1-alpha))
 	.print('----------------------------------------------------------------')
 	.print('Linear model')
-	tmp = CochranArmitage_CI_rx2(n, a, alpha, 0)
-	betahat = tmp$estimate
-	L = tmp$lower
-	U = tmp$upper
+	tmp <- CochranArmitage_CI_rx2(n, a, alpha, 0)
+	betahat <- tmp$estimate
+	L <- tmp$lower
+	U <- tmp$upper
 	.print('  Cochran-Armitage CI   %7.5f    %7.5f to %7.5f   %6.4f', betahat, L, U, U-L)
 	.print('  Wald CI               %7.5f    %7.5f to %7.5f   %6.4f', results_linear$betahat, results_linear$CI_Wald[1], results_linear$CI_Wald[2], results_linear$CI_Wald_width)
 	.print('Logit model')
@@ -114,7 +111,6 @@ the_rx2_table = function(n, alpha=0.05, direction="increasing") {
 	.print('----------------------------------------------------------------')
 }
 
-.print = function(s, ...) {
+.print <- function(s, ...) {
 	print(sprintf(gsub('\n','',s), ...), quote=F)
 }
-
