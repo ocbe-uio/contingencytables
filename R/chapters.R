b3d46d2a657b968396b890202c3747703a989d92
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-08-30 12:36:07 +0200

    Added function to chapter list

--- R/chapters.R
+++ R/chapters.R
@@ -205,7 +205,9 @@ list_chapter_functions <- function() {
 			Pearson_correlation_coefficient_rxc_bca,
 		Pearson_LR_tests_rxc = Pearson_LR_tests_rxc,
 		Pearson_residuals_rxc = Pearson_residuals_rxc,
-		Scheffe_type_CIs_rxc = Scheffe_type_CIs_rxc
+		Scheffe_type_CIs_rxc = Scheffe_type_CIs_rxc,
+		Spearman_correlation_coefficient_rxc =
+			Spearman_correlation_coefficient_rxc
 	)
 	ch8 <- list(
 		MOVER_Wilson_score_CI_paired_2x2 = MOVER_Wilson_score_CI_paired_2x2,
