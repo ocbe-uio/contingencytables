e66010082fffc273e351b38034d0f4d4658fb5a4
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-08-30 10:33:28 +0200

    Reformatted function

--- R/Kendalls_tau_b_rxc.R
+++ R/Kendalls_tau_b_rxc.R
@@ -1,65 +1,72 @@
-# function [tau_b, L, U] = Kendalls_tau_b_rxc(n, alpha, printresults)
+#' @title Kendall's tau-b with confidence interval based on the Fieller standard deviation
+#' @description Kendall's tau-b with confidence interval based on the Fieller standard deviation
+#' @description Described in Chapter 7 "The rxc Table"
+#' @param Dependencies gamma_coefficient_rxc.m
+#' @param n the observed table (an rxc matrix)
+#' @param alpha the nominal significance level, used to compute a 100(1-alpha)% confidence interval
+#' @param printresults display results (FALSE = no, TRUE = yes)
+#' @examples load_chapter(7)
+#' # Colorectal cancer (Table 7.7)
+#' n <- rbind(
+#'   c(2, 4, 29, 19), c(7, 6, 116, 51), c(19, 27, 201, 76), c(18, 22, 133, 54)
+#' )
+#' Kendalls_tau_b_rxc(n)
+#'
+#' # Breast Tumor (Table 7.8)
+#' n <- matrix(
+#'   c(15, 35, 6, 9, 6, 2, 4, 2, 11, 11, 0, 0, 1, 10, 21),
+#'   ncol = 5, byrow = TRUE
+#' )
+#' Kendalls_tau_b_rxc(n)
+#'
+#' # Self-rated health (Table 7.9)
+#' n <- matrix(
+#'   c(2, 3, 3, 3, 2, 58, 98, 14, 8, 162, 949, 252, 4, 48, 373, 369),
+#'   ncol = 4, byrow = TRUE
+#' )
+#' Kendalls_tau_b_rxc(n)
+#'
+#' unload_chapter(7)
+Kendalls_tau_b_rxc <- function(n, alpha = 0.05, printresults = TRUE) {
+	nip <- apply(n, 1, sum)
+	npj <- apply(n, 2, sum)
+	N <- sum(n)
 
-Kendalls_tau_b_rxc = function(n, alpha=0.05, printresults=T) {
-
-	# Kendall's tau-b with confidence interval based on the Fieller standard deviation
-	# Described in Chapter 7 "The rxc Table"
-	# 
-	# Dependencies: gamma_coefficient_rxc.m
-	#
-	# Input arguments
-	# ---------------
-	# n: the observed table (an rxc matrix)
-	# alpha: the nominal significance level, used to compute a 100(1-alpha)% confidence interval
-	# printresults: display results (F = no, T = yes)
-	
-	if (missing(n)) {
-	    n = rbind(c(2,4,29,19),c(7,6,116,51),c(19,27,201,76),c(18,22,133,54)) # Colorectal cancer (Table 7.7)
-		# n = [15 35 6 9 6; 2 4 2 11 11; 0 0 1 10 21] # Breast Tumor (Table 7.8)
-		# n = [2 3 3 3; 2 58 98 14; 8 162 949 252; 4 48 373 369] # Self-rated health (Table 7.9)
-	}
-		
-	nip = apply(n, 1, sum)
-	npj = apply(n, 2, sum)
-	N = sum(n)
-	
 	# Get the number of concordant and discordant pairs from the gamma
 	# coefficient
 	# [~, C, D] = gamma_coefficient_rxc(n, 0);
-	tmp = gamma_coefficient_rxc(n, 0)
-	C = tmp[[2]]
-	D = tmp[[3]]
-	
+	tmp <- gamma_coefficient_rxc(n, 0)
+	C <- tmp[[2]]
+	D <- tmp[[3]]
+
 	# The number of pairs tied on the rows
-	T1 = sum(nip*(nip-1)/2)
-	
+	T1 <- sum(nip * (nip - 1) / 2)
+
 	# The number of pairs tied on the columns
-	T2 = sum(npj*(npj-1)/2)
-	
+	T2 <- sum(npj * (npj - 1) / 2)
+
 	# Kendall's tau-b
-	tau_b = (C - D)/sqrt((N*(N-1)/2 - T1) * (N*(N-1)/2 - T2))
-	
+	tau_b <- (C - D) / sqrt((N * (N - 1) / 2 - T1) * (N * (N - 1) / 2 - T2))
+
 	# Fisher Z transformation
-	z = atanh(tau_b)
+	z <- atanh(tau_b)
 	# Or, equivalently
-	# z = 0.5*log((1+tau_b)/(1-tau_b))
-	
+	# z = 0.5 * log((1 + tau_b) / (1-tau_b))
+
 	# The 1-alpha percentile of the standard normal distribution
-	z_alpha = qnorm(1-alpha/2, 0, 1)
-	
+	z_alpha <- qnorm(1 - alpha / 2, 0, 1)
+
 	# Confidence limits with the Fieller standard deviation
-	l = z - z_alpha*sqrt(0.437/(N - 4))
-	u = z + z_alpha*sqrt(0.437/(N - 4))
-	
+	l <- z - z_alpha * sqrt(0.437 / (N - 4))
+	u <- z + z_alpha * sqrt(0.437 / (N - 4))
+
 	# Back transform to obtain the CIs for Kendall's tau-b
-	L = (exp(2*l) - 1)/(exp(2*l) + 1)
-	U = (exp(2*u) - 1)/(exp(2*u) + 1)
-	
+	L <- (exp(2 * l) - 1) / (exp(2 * l) + 1)
+	U <- (exp(2 * u) - 1) / (exp(2 * u) + 1)
+
 	if (printresults) {
-	    print(sprintf('Kendalls tau-b w/Fieller CI: tau-b = %7.4f (%g%% CI %7.4f to %7.4f)', tau_b, 100*(1-alpha), L, U), quote=F)
+		print(sprintf("Kendalls tau-b w / Fieller CI: tau-b = %7.4f (%g%% CI %7.4f to %7.4f)", tau_b, 100 * (1 - alpha), L, U), quote = FALSE)
 	}
-	
-	invisible(list(tau_b=tau_b, L=L, U=U))
+
+	invisible(list(tau_b = tau_b, L = L, U = U))
 }
-	
-	
