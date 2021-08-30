717279ec486666c01e11c49adad907631cf14cf4
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-08-30 07:24:56 +0200

    Added unit tests

--- tests/testthat/test-ch7.R
+++ tests/testthat/test-ch7.R
@@ -4,6 +4,14 @@ test_that("Chapter 7 functions basically work", {
 	load_chapter(7)
 	n <- matrix(c(40, 25, 54, 7, 63, 10), byrow=TRUE, ncol=2)
 	expect_output(
+		object = Bonferroni_type_CIs_rxc(n),
+		regexp = "pi_1|1 - pi_1|3: estimate = -0.2476 \\(-0.4213 to -0.0740\\)"
+	)
+	# expect_output(
+	# 	object =
+	# 	regexp =
+	# )
+	expect_output(
 		object = Pearson_LR_tests_rxc(n),
 		regexp = "Pearson chi-squared test: T = 17.562, df = 2, P = 0.00015"
 	)
