75e595cc84c655409ef542a007d84618f502313a
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-10-28 10:21:16 +0200

    Added original script

---
+++ R/the_paired_2x2_table_CIs_difference.R
@@ -0,0 +1,58 @@
+path = "/Users/ole/Desktop/Petter Laake/chap8_translation"
+source(paste(path, "Wald_CI_diff_paired_2x2.R", sep="/"))
+source(paste(path, "Wald_CI_diff_CC_paired_2x2.R", sep="/"))
+source(paste(path, "Wald_CI_AgrestiMin_paired_2x2.R", sep="/"))
+source(paste(path, "Wald_CI_BonettPrice_paired_2x2.R", sep="/"))
+source(paste(path, "Newcombe_square_and_add_CI_paired_2x2.R", sep="/"))
+source(paste(path, "Tango_asymptotic_score_CI_paired_2x2.R", sep="/"))
+
+the_paired_2x2_table_CIs_difference = function(n, alpha=0.05) {
+ 	
+ 	if (missing(n)) {
+ 	#    n = [1 1; 7 12]; # Airway hyper-responsiveness before and after stem cell transplantation (Bentur et al., 2009)
+ 	    n = rbind(c(59,6),c(16,80)) # Complete response before and after consolidation therapy (Cavo et al., 2012)
+ 	}
+ 	
+ 	N = sum(n)
+ 	
+ 	pi1phat = (n[1,1] + n[1,2])/N
+ 	pip1hat = (n[1,1] + n[2,1])/N
+ 	deltahat = pi1phat - pip1hat
+ 	
+ 	.print('\nEstimate of pi_1+: %i/%i = %5.3f\n', n[1,1] + n[1,2], N, pi1phat)
+ 	.print('Estimate of pi_+1: %i/%i = %5.3f\n\n', n[1,1] + n[2,1], N, pip1hat)
+ 	.print('Estimate of delta = pi_1+ - pi_+1: %5.3f\n\n', deltahat)
+ 	
+ 	.print('Interval method                           %i%% CI         width\n', 100*(1-alpha))
+ 	print('--------------------------------------------------------------', quote=F)
+ 	
+ 	tmp = Wald_CI_diff_paired_2x2(n, alpha, F)
+	L = tmp[[1]]; U = tmp[[2]]
+ 	.print('Wald                                %7.4f to %7.4f %7.3f\n', L, U, U - L)
+ 	
+ 	tmp = Wald_CI_diff_CC_paired_2x2(n, alpha, F)
+	L = tmp[[1]]; U = tmp[[2]]
+ 	.print('Wald w/continuity correction        %7.4f to %7.4f %7.3f\n', L, U, U - L)
+ 	
+ 	tmp = Wald_CI_AgrestiMin_paired_2x2(n, alpha, F)
+	L = tmp[[1]]; U = tmp[[2]]
+ 	.print('Wald w/Agresti-Min adjustment       %7.4f to %7.4f %7.3f\n', L, U, U - L)
+ 	
+ 	tmp = Wald_CI_BonettPrice_paired_2x2(n, alpha, F)
+	L = tmp[[1]]; U = tmp[[2]]
+ 	.print('Wald w/Bonett-Price adjustment      %7.4f to %7.4f %7.3f\n', L, U, U - L)
+ 	
+ 	tmp = Newcombe_square_and_add_CI_paired_2x2(n, alpha, F)
+	L = tmp[[1]]; U = tmp[[2]]
+ 	.print('Newcombe square-and-add             %7.4f to %7.4f %7.3f\n', L, U, U - L)
+ 	
+ 	tmp = Tango_asymptotic_score_CI_paired_2x2(n, alpha, F)
+	L = tmp[[1]]; U = tmp[[2]]
+ 	.print('Tango asymptotic score              %7.4f to %7.4f %7.3f\n', L, U, U - L)
+ 	
+ 	print('--------------------------------------------------------------', quote=F)	
+ }
+ 
+.print = function(s, ...) {
+	   print(sprintf(gsub('\n','',s), ...), quote=F)
+}
