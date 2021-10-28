aecc74c6fe99aad2443ba43dccee2bbada06a6e2
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-10-28 13:32:52 +0200

    Added original script

---
+++ R/the_paired_2x2_table_tests.R
@@ -0,0 +1,55 @@
+path = "/Users/ole/Desktop/Petter Laake/chap8_translation"
+source(paste(path, "McNemar_asymptotic_test_paired_2x2.R", sep="/"))
+source(paste(path, "McNemar_asymptotic_test_CC_paired_2x2.R", sep="/"))
+source(paste(path, "McNemar_exact_cond_test_paired_2x2.R", sep="/"))
+source(paste(path, "McNemar_midP_test_paired_2x2.R", sep="/"))
+source(paste(path, "McNemar_exact_unconditional_test_paired_2x2.R", sep="/"))
+source(paste(path, "ClopperPearson_exact_CI_1x2_beta_version.R", sep="/"))
+
+the_paired_2x2_table_tests = function(n, gamma=0.0001) {
+
+ 	if (missing(n)) {
+ 	    n = rbind(c(1,1),c(7,12)) # Airway hyper-responsiveness before and after stem cell transplantation (Bentur et al., 2009)
+ 		#    n = [59 6; 16 80]; # Complete response before and after consolidation therapy (Cavo et al., 2012)
+ 		#    n = [7 25; 2 68]; # Floppy eyelid syndrome vs obstructive sleep apnea (Ezra et al., 2010)
+ 	}
+ 	
+ 	N = sum(n)
+ 	
+ 	pi1phat = (n[1,1] + n[1,2])/N
+ 	pip1hat = (n[1,1] + n[2,1])/N
+ 	
+ 	.print('\nH_0: pi_1+ = pi_+1  vs  H_A: pi_1+ ~= pi_+1\n\n')
+ 	.print('Estimate of pi_1+: %i/%i = %5.3f\n', n[1,1] + n[1,2], N, pi1phat)
+ 	.print('Estimate of pi_+1: %i/%i = %5.3f\n\n', n[1,1] + n[2,1], N, pip1hat)
+ 	
+ 	print('Test                             P-value  (test statistic)', quote=F)
+ 	print('---------------------------------------------------------', quote=F)
+ 	
+ 	tmp = McNemar_asymptotic_test_paired_2x2(n, F)
+	P = tmp[[1]]; Z = tmp[[2]]
+ 	.print('McNemar asymptotic               %6.4f   (Z = %5.3f)\n', P, Z)
+ 	
+ 	tmp = McNemar_asymptotic_test_CC_paired_2x2(n, F)
+	P = tmp[[1]]; Z = tmp[[2]]
+ 	.print('McNemar asymptotic w/CC          %6.4f   (Z = %5.3f)\n', P, Z)
+ 	
+ 	P = McNemar_exact_cond_test_paired_2x2(n, F)
+ 	.print('McNemar exact conditional        %6.4f\n', P)
+ 	
+ 	P = McNemar_midP_test_paired_2x2(n, F)
+ 	.print('McNemar mid-P                    %6.4f\n', P)
+ 	
+ 	P = McNemar_exact_unconditional_test_paired_2x2(n, 0, F)
+ 	.print('McNemar exact unconditional      %6.4f\n', P)
+ 	
+ 	P = McNemar_exact_unconditional_test_paired_2x2(n, gamma, F)
+ 	.print('McNemar exact unconditional*     %6.4f\n', P)
+ 	
+ 	print('---------------------------------------------------------', quote=F)
+ 	.print('*gamma = %-10.8g\n', gamma)
+ }
+ 	
+.print = function(s, ...) {
+	   print(sprintf(gsub('\n','',s), ...), quote=F)
+}
