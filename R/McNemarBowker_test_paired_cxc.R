74e05f50b5aee6610a743a524d7929d9fa957a33
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-12-07 08:38:25 +0100

    Added original script

---
+++ R/McNemarBowker_test_paired_cxc.R
@@ -0,0 +1,41 @@
+McNemarBowker_test_paired_cxc = function(n, printresults=T) {
+ 	# The McNemar-Bowker test for marginal symmetry
+ 	# Described in Chapter 9 "The Paired cxc Table"
+ 	#
+ 	# Input arguments
+ 	# ---------------
+ 	# n: the observed table (a cxc matrix)
+ 	# printresults: display results (F = no, T = yes)
+ 	
+ 	if (missing(n)) {
+ 	    # Pretherapy susceptability of pathogens (Peterson et al., 2007)
+ 	    n = rbind(c(596,18,6,5), c(0,2,0,0), c(0,0,42,0), c(11,0,0,0))
+ 	}
+ 	
+ 	c = nrow(n)
+ 	
+ 	# The McNemar-Bowker test statistic
+ 	T0 = 0
+ 	for (i in 2:c) {
+ 	    for (j in 1:(i-1)) {
+ 	        if (n[i,j] + n[j,i] > 0) {
+ 	            T0 = T0 + ((n[i,j] - n[j,i])^2)/(n[i,j] + n[j,i])
+ 	        }
+ 	    }
+ 	}
+ 	
+ 	# Reference distribution: chi-squared with c*(c-1)/2 degrees of freedom
+ 	df = c*(c - 1)/2
+ 	P = 1 - pchisq(T0, df)
+ 	
+ 	if (printresults) {
+     .print('The McNemar-Bowker test for symmetry: P = %8.6f, T0 = %6.3f (df=%g)\n', P, T0, df)
+ 	}
+ 	
+ 	invisible(list(P=P,T=T0,df=df))
+ }
+
+ 	
+ .print = function(s, ...) {
+	   print(sprintf(gsub('\n','',s), ...), quote=F)
+ }
