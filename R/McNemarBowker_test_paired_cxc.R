0cbb935cc16e7d9a1ee9442282e98e45b37d93bd
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-12-07 08:40:39 +0100

    Reformatted function

--- R/McNemarBowker_test_paired_cxc.R
+++ R/McNemarBowker_test_paired_cxc.R
@@ -1,41 +1,38 @@
-McNemarBowker_test_paired_cxc = function(n, printresults=T) {
- 	# The McNemar-Bowker test for marginal symmetry
- 	# Described in Chapter 9 "The Paired cxc Table"
- 	#
- 	# Input arguments
- 	# ---------------
- 	# n: the observed table (a cxc matrix)
- 	# printresults: display results (F = no, T = yes)
- 	
- 	if (missing(n)) {
- 	    # Pretherapy susceptability of pathogens (Peterson et al., 2007)
- 	    n = rbind(c(596,18,6,5), c(0,2,0,0), c(0,0,42,0), c(11,0,0,0))
- 	}
- 	
- 	c = nrow(n)
- 	
- 	# The McNemar-Bowker test statistic
- 	T0 = 0
- 	for (i in 2:c) {
- 	    for (j in 1:(i-1)) {
- 	        if (n[i,j] + n[j,i] > 0) {
- 	            T0 = T0 + ((n[i,j] - n[j,i])^2)/(n[i,j] + n[j,i])
- 	        }
- 	    }
- 	}
- 	
- 	# Reference distribution: chi-squared with c*(c-1)/2 degrees of freedom
- 	df = c*(c - 1)/2
- 	P = 1 - pchisq(T0, df)
- 	
- 	if (printresults) {
-     .print('The McNemar-Bowker test for symmetry: P = %8.6f, T0 = %6.3f (df=%g)\n', P, T0, df)
- 	}
- 	
- 	invisible(list(P=P,T=T0,df=df))
- }
+#' @title The McNemar-Bowker test for marginal symmetry
+#' @description The McNemar-Bowker test for marginal symmetry
+#' @description Described in Chapter 9 "The Paired cxc Table"
+#' @param n the observed table (a cxc matrix)
+#' @param printresults display results (FALSE = no, TRUE = yes)
+#' @examples load_chapter(9)
+#' # Pretherapy susceptability of pathogens (Peterson et al., 2007)
+#' n <- rbind(c(596, 18, 6, 5), c(0, 2, 0, 0), c(0, 0, 42, 0), c(11, 0, 0, 0))
+#' McNemarBowker_test_paired_cxc(n)
+#' unload_chapter(9)
+McNemarBowker_test_paired_cxc <- function(n, printresults = TRUE) {
+  c <- nrow(n)
 
- 	
- .print = function(s, ...) {
-	   print(sprintf(gsub('\n','',s), ...), quote=F)
- }
+  # The McNemar-Bowker test statistic
+  T0 <- 0
+  for (i in 2:c) {
+    for (j in 1:(i - 1)) {
+      if (n[i, j] + n[j, i] > 0) {
+        T0 <- T0 + ((n[i, j] - n[j, i])^2) / (n[i, j] + n[j, i])
+      }
+    }
+  }
+
+  # Reference distribution: chi-squared with c * (c-1) / 2 degrees of freedom
+  df <- c * (c - 1) / 2
+  P <- 1 - pchisq(T0, df)
+
+  if (printresults) {
+    .print("The McNemar-Bowker test for symmetry: P = %8.6f, T0 = %6.3f (df=%g)\n", P, T0, df)
+  }
+
+  invisible(list(P = P, T = T0, df = df))
+}
+
+
+.print <- function(s, ...) {
+  print(sprintf(gsub("\n", "", s), ...), quote = FALSE)
+}
