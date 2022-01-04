a1cdd98fb661efa94c6b79ea12e561e6decd60ec
Waldir Leoncio <w.l.netto@medisin.uio.no>
2022-01-04 16:58:21 +0100

    Added original script

---
+++ R/RBG_test_and_CI_stratified_2x2.R
@@ -0,0 +1,69 @@
+ RBG_test_and_CI_stratified_2x2 = function(n, alpha=0.05, printresults=T) {
+ 	# The RBG test and CI for a common odds ratio
+ 	# (A Wald-type test and CI based on the Mantel-Haenszel estimate)
+ 	# Described in Chapter 10 "Stratified 2x2 Tables and Meta-Analysis"
+ 	#
+ 	# Dependencies
+ 	# ------------
+ 	# "MantelHaenszel_estimate_stratified_2x2.m"
+ 	#
+ 	# Input arguments
+ 	# ---------------
+ 	# n: the observed table (a 2x2xk matrix, where k is the number of strata)
+ 	# alpha: the nominal level, e.g. 0.05 for 95# CIs
+ 	# printresults: display results (F = no, T = yes)
+ 	
+ 	if (missing(n)) {
+ 	    # Smoking and lung cancer (Doll and Hill, 1950)
+ 		#  n = zeros(2, 2, 2)
+ 		#  n(:,:,1) = [647 622; 2 27]
+ 		#  n(:,:,2) = [41 28; 19 32]
+ 	
+ 	    # Prophylactice use of Lidocaine in myocardial infarction (Hine et al., 1989)
+	    n = array(0, dim=c(2,2,6))
+ 	    n[,,1] = rbind(c(2,37),c(1,42))
+ 	    n[,,2] = rbind(c(4,40),c(4,40))
+ 	    n[,,3] = rbind(c(6,101),c(4,106))
+ 	    n[,,4] = rbind(c(7,96),c(5,95))
+ 	    n[,,5] = rbind(c(7,103),c(3,103))
+ 	    n[,,6] = rbind(c(11,143),c(4,142))
+ 	}
+ 	
+ 	nppk = apply(n, 3, sum)
+ 	
+ 	# Get the Mantel-Haenszel overall estimate
+ 	thetahatMH  = MantelHaenszel_estimate_stratified_2x2(n, 'logit', F)[[1]]
+ 	
+ 	# Estimate the standard error
+ 	A = sum(n[1,1,]*n[2,2,]/nppk)
+ 	B = sum(n[1,2,]*n[2,1,]/nppk)
+ 	C = sum((n[1,1,] + n[2,2,])*n[1,1,]*n[2,2,]/(nppk^2))
+ 	D = sum((n[1,1,] + n[2,2,])*n[1,2,]*n[2,1,]/(nppk^2))
+ 	E = sum((n[1,2,] + n[2,1,])*n[1,1,]*n[2,2,]/(nppk^2))
+ 	F = sum((n[1,2,] + n[2,2,])*n[1,2,]*n[2,1,]/(nppk^2))
+ 	SElog = sqrt(C/(2*A^2) + (D + E)/(2*A*B) + F/(2*B^2))
+ 	
+ 	# The Wald test statistic
+ 	Z = log(thetahatMH)/SElog
+ 	
+ 	# The two-sided P-value (reference distribution: standard normal)
+ 	P = 2*(1 - pnorm(abs(Z), 0, 1))
+ 	
+ 	# The upper alpha/2 percentile of the standard normal distribution
+ 	z_alpha = qnorm(1 - alpha/2, 0, 1)
+ 	
+ 	# Calculate the confidence limits
+ 	L = thetahatMH*exp(-z_alpha*SElog)
+ 	U = thetahatMH*exp(z_alpha*SElog)
+ 	
+ 	if (printresults) {
+     .print('The RBG test: P = %7.5f, Z = %6.3f\n', P, Z)
+     .print('The RBG CI: thetahatMH = %6.4f (%g%% CI %6.4f to %6.4f)\n', thetahatMH, 100*(1 - alpha), L, U)
+ 	}
+ 	
+ 	invisible(list(P=P,Z=Z,L=L,U=U,thetahatMH=thetahatMH,SElog=SElog))
+}
+ 	
+ .print = function(s, ...) {
+	   print(sprintf(gsub('\n','',s), ...), quote=F)
+ }
