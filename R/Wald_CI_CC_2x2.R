15e711f4560e6a3dd37afd6af021c588591b42c7
Waldir Leoncio <w.l.netto@medisin.uio.no>
2022-07-21 09:18:30 +0200

    Fixed syntax, updated docs

--- R/Wald_CI_CC_2x2.R
+++ R/Wald_CI_CC_2x2.R
@@ -48,7 +48,7 @@ Wald_CI_CC_2x2 <- function(n, alpha = 0.05, printresults = TRUE) {
       sprintf(
         paste(
           "The Wald CI with continuity correction: estimate =",
-          "%6.4f (%g%% CI %6.4f to %6.4f)",
+          "%6.4f (%g%% CI %6.4f to %6.4f)"
         ),
         estimate, 100 * (1 - alpha), L, U
       ),
