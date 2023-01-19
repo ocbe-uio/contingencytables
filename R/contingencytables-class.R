233fd9ae11ad9e32f01a881654051ef1ecd65eac
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-01-19 11:16:58 +0100

    Added generator for package classes (#31)

---
+++ R/contingencytables-class.R
@@ -0,0 +1,32 @@
+# Instantiates an object of the "contingencytables_*" classes
+newContingencytablesOutput <- function(content, class_name) {
+  # Defining valid structure
+  valid_classes <- c(
+    "contingencytables_singletest",
+    "contingencytables_multipletests"
+  )
+  valid_content <- switch(
+    class_name,
+    "contingencytables_singletest"   = c("name", "statistics"),
+    "contingencytables_multipletests" = c("statistics", "FUN"),
+    stop(
+      "Invalid class_name. Please choose between ",
+      paste(valid_classes, collapse = ", ")
+    )
+  )
+
+  # Validating input
+  names_match <- all(sort(names(content)) %in% sort(valid_content))
+  if (is.null(names(content)) || !names_match) {
+    stop(
+      "Invalid content. List must contain the following elements: ",
+      paste(valid_content, collapse = ", ")
+    )
+  } else {
+    if (class_name == "contingencytables_multipletests") {
+      FUN_arguments <- names(formals(content$FUN))
+      stopifnot(FUN_arguments == "statistics")
+    }
+    return(structure(content, class = class_name))
+  }
+}
