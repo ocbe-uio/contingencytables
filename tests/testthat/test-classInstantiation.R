3e5480fbc6e22b9ae7c2bcb5287d82474285f69b
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-01-20 10:25:57 +0100

    Removed superseded class, updated tests (#31)

--- tests/testthat/test-classInstantiation.R
+++ tests/testthat/test-classInstantiation.R
@@ -1,54 +1,16 @@
 context("Class instantiation")
 
-single_name <- "contingencytables_singletest"
-multiple_name <- "contingencytables_multipletests"
-single_content <- list(name = "test", statistics = pi)
-multiple_content <- list(statistics = pi, FUN = function(statistics) "hi")
+x <- list(
+  Adjusted_inv_sinh_CI_OR_2x2(lampasona_2013),
+  BaptistaPike_exact_conditional_CI_2x2(tea),
+  CochranArmitage_MH_tests_rx2(indredavik_2008, c(1, 2, 3, 4, 5)),
+  with(snp6498169$subset, Exact_multinomial_test_1xc(n, pi0))
+)
 
-test_that("Invalid classes are rejected", {
-  expect_error(
-    newContingencytablesOutput(single_content, "this is fine"),
-    "Invalid class_name"
+test_that("Objects have correct class elements", {
+  expect_equal(vapply(x, class, ""), rep("contingencytables_result", length(x)))
+  expect_equal(
+    vapply(x, names, c("", "")),
+    matrix(rep(c("statistics", "print_format"), length(x)), 2)
   )
-  expect_error(
-    newContingencytablesOutput(multiple_content, "numeric"),
-    "Invalid class_name"
-  )
-})
-
-test_that("Invalid content is rejected", {
-  expect_error(
-    newContingencytablesOutput(pi, single_name),
-    "Invalid content\\. .+ name, statistics"
-  )
-  expect_error(
-    newContingencytablesOutput(pi, multiple_name),
-    "Invalid content\\. .+ statistics, FUN"
-  )
-  expect_error(
-    newContingencytablesOutput(list(statistics = 1, FUN = sd), multiple_name),
-    "FUN_arguments == \"statistics\" are not all TRUE"
-  )
-})
-
-test_that("Invalid combinations are rejected", {
-  expect_error(
-    newContingencytablesOutput(single_content, multiple_name),
-    "Invalid content\\. .+ statistics, FUN"
-  )
-  expect_error(
-    newContingencytablesOutput(multiple_content, single_name),
-    "Invalid content\\. .+ name, statistics"
-  )
-})
-
-test_that("Valid content is returned and printed correctly", {
-  s <- newContingencytablesOutput(single_content, single_name)
-  m <- newContingencytablesOutput(multiple_content, multiple_name)
-  expect_s3_class(s, single_name)
-  expect_s3_class(m, multiple_name)
-  expect_equal(s, structure(single_content, class = single_name))
-  expect_equal(m, structure(multiple_content, class = multiple_name))
-  expect_output(print(s))
-  expect_output(print(m))
 })
