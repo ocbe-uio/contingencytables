46572b3c1e0e557eb400f3aba9058aaf31ff7dd8
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-02-12 12:29:59 +0100

    Added validation to plotting function (#53)

--- R/plot-functions.R
+++ R/plot-functions.R
@@ -1,8 +1,20 @@
 #' @export
 plot.contingencytables_result <- function(x, y, ...) {
+  # Validating  ================================================================
+  if (!identical(names(x), c("P", "p_values", "pi_values"))) {
+    stop(
+      "Plotting is only supported for the following functions:\n",
+      "    - Exact_unconditional_test_2x2()\n",
+      "    - McNemar_exact_unconditional_test_paired_2x2()"
+    )
+  }
+
+  # Calculating plotting elements ==============================================
   P <- max(x$p_values)
   index <- which(P == x$p_values)
   common_pi_at_max_value <- x$pi_values[index]
+
+  # Plotting ===================================================================
   plot(x$pi_values, x$p_values, lty = 1, col = "black", ...)
   lines(
     c(common_pi_at_max_value, common_pi_at_max_value), c(0, P),
