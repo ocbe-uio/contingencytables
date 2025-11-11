3b875b28f126722f6b59693906623f10638db0a0
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-11-11 12:00:51 +0100

    🐞 Fix: extend chap4 section to chaps 8 and 10 (#57)

--- R/McNemar_asymptotic_test_CC_paired_2x2.R
+++ R/McNemar_asymptotic_test_CC_paired_2x2.R
@@ -8,7 +8,7 @@
 #' McNemar_asymptotic_test_CC_paired_2x2(ezra_2010)
 #'
 #' @export
-#' @inheritSection chap4 Table orientation
+#' @inheritSection chap8 Table orientation
 #' @return An object of the [contingencytables_result] class,
 #' basically a subclass of [base::list()]. Use the [utils::str()] function
 #' to see the specific elements returned.
--- R/chapters.R
+++ R/chapters.R
@@ -232,6 +232,11 @@ NULL
 #'    \item{the_paired_2x2_table_tests}
 #' }
 #' @note You can also print the list above with \code{list_functions(8)}.
+#' @section Table orientation: In most study designs, rows designate a grouping of the data, for
+#' instance, into treatment or exposure groups, while the columns indicate a
+#' dichotomous outcome, usually with the first column representing the outcome
+#' of interest. This designation of rows and columns may not be relevant in all
+#' study designs, please see the introduction to chapter 8 for details.
 #' @references
 #' - Fagerland MW, Lydersen S, Laake P (2017) Statistical Analysis of
 #' Contingency Tables. Chapman & Hall/CRC, Boca Raton, FL
@@ -285,6 +290,11 @@ NULL
 #'    \item{stratified_2x2_tables}
 #' }
 #' @note You can also print the list above with \code{list_functions(10)}.
+#' @section Table orientation: In most study designs, rows designate a grouping of the data, for
+#' instance, into treatment or exposure groups, while the columns indicate a
+#' dichotomous outcome, usually with the first column representing the outcome
+#' of interest. This designation of rows and columns may not be relevant in all
+#' study designs, please see the introduction to chapter 10 for details.
 #' @references
 #' - Fagerland MW, Lydersen S, Laake P (2017) Statistical Analysis of
 #' Contingency Tables. Chapman & Hall/CRC, Boca Raton, FL
